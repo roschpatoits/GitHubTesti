1e6579df4889f0ddd5469a0cc134d30a3bc410cb
Petri kaistinen <petri.kaistinen@gmail.com>
2018-04-28 22:42:09 +0300

    pööpöö

--- testi/testi/Program.cs
+++ testi/testi/Program.cs
@@ -18,6 +18,7 @@ namespace testi
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             ///Pööö
+            //pööpöö
         }
     }
 }
